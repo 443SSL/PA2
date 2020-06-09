6e923110426d139a42f93b59d7532b5afe3187bf
Jay <jdlenner3@gmail.com>
2020-06-09 01:37:54 -0500

    trim finished

--- shell.cpp
+++ shell.cpp
@@ -2,9 +2,47 @@
 #include <iostream>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <vector>
+#include <string.h>
 #include <unistd.h>
 
 using namespace std;
+
+string trim (string input){
+    int i = 0;
+
+    while(i < input.size() && input[i] == ' '){
+        i += 1;
+    }
+    if(i < input.size()){
+        input = input.substr(i);
+    } else {
+        return "";
+    }
+
+    int i = input.size() - 1;
+    while(i >= 0 && input[i] == ' '){
+        i -= 1;
+    }
+    if(i >= 0){
+        input = input.substr(0, i + 1);
+    } else {
+        return "";
+    }
+
+    return input;
+
+}
+
+vector<string> split (string line, string sep=" "){
+    vector<string> result_args;
+
+    while(line.size()){
+                    
+    }
+    return result_args;
+}
 
 int main (){
     while (true){
